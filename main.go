47d51605cd1b14f5e9d4d385775766124c6dcfed
kou12345 <hi.0412.ko.2002@gmail.com>
2023-03-01 00:24:58 +0900

    run関数のテストを実装

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ func run(ctx context.Context) error {
 	s := &http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
-	// 別ゴルーチンでHTTPサーバを起動する
+	// 別ゴルーチンでHTTPサーバーを起動する
 	eg.Go(func() error {
-		// http.ErrServerClosedは
-		// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
+		// http.ErrServerClosed は
+		// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
 		if err := s.ListenAndServe(); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
@@ -34,12 +34,11 @@ func run(ctx context.Context) error {
 		}
 		return nil
 	})
-
 	// チャネルからの通知（終了通知）を待機する
 	<-ctx.Done()
 	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdow: %+v", err)
+		log.Printf("failed to shutdown: %+v", err)
 	}
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
+	// Goメソッドで起動した別ゴルーチンの終了を待つ。
 	return eg.Wait()
 }
