1f0421404e03f376f5f2ea15d99f9bcdbb741229
kou12345 <hi.0412.ko.2002@gmail.com>
2023-04-14 16:50:07 +0900

    任意のポートで起動できるように変更

--- main.go
+++ main.go
@@ -4,22 +4,35 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	port := os.Args[1]
+	l, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Printf("failed to listen port %s: %v", port, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
+		// 引数で受け取ったnet.Listenerを利用するので
+		// Addrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
@@ -27,7 +40,7 @@ func run(ctx context.Context) error {
 	eg.Go(func() error {
 		// http.ErrServerClosed は
 		// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
